e3b07a36165338b3681a0c42243b93a5b63c309e
gorkemguzel32 <97730415+gorkemguzel32@users.noreply.github.com>
2022-06-25 13:24:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setuptools.setup(
     author_email="guzelgo17@itu.edu.tr",
     packages=setuptools.setuptools.find_packages(),
     long_description="I developed a program that creates database from a text file for my evaluation process at Organon Analytics.",
-    download_url=""
+    download_url="https://github.com/gorkemguzel32/db_creator_gorkemguzel/archive/refs/tags/v1.0.0.tar.gz"
 )
